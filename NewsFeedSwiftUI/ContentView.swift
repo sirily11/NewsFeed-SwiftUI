5ff8652ad1dce94000dff5e4c7c2233180a73817
sirily11 <sirily1997@gmail.com>
2020-10-24 15:27:08 +0800

    Initial Commit

---
+++ NewsFeedSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsFeedSwiftUI
+//
+//  Created by 李其炜 on 10/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsFeedSwiftUI/NewsFeedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedSwiftUIApp.swift
+//  NewsFeedSwiftUI
+//
+//  Created by 李其炜 on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
